5d3b8bb7254547158c1d854afe46d45abb304034
Ahmed Omar <ahmed.omar.platform@gmail.com>
2025-10-02 10:01:08 +0300

    added db integration using neon and prisma as orm, added data models

--- src/components/auth/register-form.tsx
+++ src/components/auth/register-form.tsx
@@ -7,7 +7,7 @@ import { useState, useTransition } from "react";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 
-import { LoginSchema } from "@/schemas";
+import { RegisterSchema } from "@/schemas";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
@@ -24,28 +24,29 @@ import { CardWrapper } from "@/components/auth/card-wrapper";
 
 import { FormError } from "@/components/form-error";
 import { FormSuccess } from "@/components/form-succes";
-import { login } from "../../../actions/login";
+import { register } from "../../../actions/register";
 
-export const LoginForm = () => {
+export const RegisterForm = () => {
   const [error, setError] = useState<string | undefined>("");
   const [success, setSuccess] = useState<string | undefined>("");
 
   const [isPending, startTransition] = useTransition();
 
-  const form = useForm<z.infer<typeof LoginSchema>>({
-    resolver: zodResolver(LoginSchema),
+  const form = useForm<z.infer<typeof RegisterSchema>>({
+    resolver: zodResolver(RegisterSchema),
     defaultValues: {
       email: "",
       password: "",
+      name: "",
     },
   });
 
-  const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+  const onSubmit = (values: z.infer<typeof RegisterSchema>) => {
     setError("");
     setSuccess("");
 
     startTransition(() => {
-      login(values).then((data) => {
+      register(values).then((data) => {
         setError(data.error);
         setSuccess(data.success);
       });
@@ -54,14 +55,31 @@ export const LoginForm = () => {
 
   return (
     <CardWrapper
-      headerLabel="Welcome Back"
-      backButtonLabel="Don't Have an account?"
-      backButtonHref="/auth/register"
+      headerLabel="Create an Account"
+      backButtonLabel="Already have an account?"
+      backButtonHref="/auth/login"
       showSocial
     >
       <Form {...form}>
         <form className="space-y-6" onSubmit={form.handleSubmit(onSubmit)}>
           <div className="space-y-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      {...field}
+                      placeholder="Your name"
+                      disabled={isPending}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
             <FormField
               control={form.control}
               name="email"
@@ -102,7 +120,7 @@ export const LoginForm = () => {
           <FormError message={error} />
           <FormSuccess message={success} />
           <Button type="submit" className="w-full" disabled={isPending}>
-            Login
+            Join Us
           </Button>
         </form>
       </Form>
--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -8,3 +8,15 @@ export const LoginSchema = z.object({
     error: "Password is Resquired",
   }),
 });
+
+export const RegisterSchema = z.object({
+  name: z.string().min(1, {
+    error: "Name is Required",
+  }),
+  email: z.email({
+    error: "Email is Required",
+  }),
+  password: z.string().min(8, {
+    error: "Minimum 8 characters",
+  }),
+});
